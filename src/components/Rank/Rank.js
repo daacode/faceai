ab4c96591a0659a5ccb9202968275d5b6ed2a99b
Daagital <adeneyeabiodun@gmail.com>
2020-03-24 21:25:15 +0100

    frontend components

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Rank = () => {
+  return (
+    <div className="ma4 mt0">
+      <div className="white f3">{"David, your current rank is ..."}</div>
+      <div className="white f1">{"#5"}</div>
+    </div>
+  );
+};
+
+export default Rank;
